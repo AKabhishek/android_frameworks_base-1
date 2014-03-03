38c6a4983bca3a2756c3217aed20a47b437390e0
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    Add toast animations [1/2]
    Change-Id: Ic4d1b573b8ed5c657e8e9b226c28944d6a0895ec
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3939,6 +3939,13 @@ public final class Settings {
         public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
         /**
+         * Toast animations
+         *
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
