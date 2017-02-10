DirtyUnicorns/android_frameworks_base

Battery tile bolt/text should not be clear
This was kind of tricky because for the statusbar, the color of
text/bolt is only set for circle style (PorterDuff.Mode OVERLAY).
All other styles simply set it to clear (PorterDuff.Mode CLEAR).
This poses issues in the QS tile because the background shown is
the app currently open.
Setting all styles to OVERLAY doesn't work either, because that
would cause the statusbar battery icon to not be consistent with
the rest of the statusbar icons.
This was solved by allowing to force blendMode to OVERLAY, so
that the QS tile could force it, without affecting the statusbar
icon. Color was then set to either white for circle style, or
the QS panel background color for all other styles.

Change-Id: I5d9bfe9f77af0082247e1053187245425b2c33ae

packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryTile.java
@@ -133,7 +133,7 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
                 }
                 BatteryMeterDrawable drawable =
                         new BatteryMeterDrawable(context, new Handler(Looper.getMainLooper()),
-                        context.getColor(R.color.batterymeter_frame_color), mBatteryStyle);
+                        context.getColor(R.color.batterymeter_frame_color), mBatteryStyle, true);
                 drawable.onBatteryLevelChanged(mLevel, mPluggedIn, mCharging);
                 drawable.onPowerSaveChanged(mPowerSave);
                 return drawable;
@@ -179,7 +179,7 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
     private final class BatteryDetail implements DetailAdapter, OnClickListener,
             OnAttachStateChangeListener {
         private final BatteryMeterDrawable mDrawable = new BatteryMeterDrawable(mHost.getContext(),
-                new Handler(), mHost.getContext().getColor(R.color.batterymeter_frame_color), mBatteryStyle);
+                new Handler(), mHost.getContext().getColor(R.color.batterymeter_frame_color), mBatteryStyle, true);
         private View mCurrentView;
 
         @Override

AKabhishek/android_frameworks_base-1

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryTile.java
@@ -144,7 +144,7 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
                 }
                 BatteryMeterDrawable drawable =
                         new BatteryMeterDrawable(context, new Handler(Looper.getMainLooper()),
-                        context.getColor(R.color.qs_batterymeter_frame_color), mBatteryStyle);
+                        context.getColor(R.color.qs_batterymeter_frame_color), mBatteryStyle, true);
                 drawable.onBatteryLevelChanged(mLevel, mPluggedIn, mCharging);
                 drawable.onPowerSaveChanged(mPowerSave);
                 return drawable;
@@ -190,7 +190,7 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
     private final class BatteryDetail implements DetailAdapter, OnClickListener,
             OnAttachStateChangeListener {
         private final BatteryMeterDrawable mDrawable = new BatteryMeterDrawable(mHost.getContext(),
-                new Handler(), mHost.getContext().getColor(R.color.qs_batterymeter_frame_color), mBatteryStyle);
+                new Handler(), mHost.getContext().getColor(R.color.qs_batterymeter_frame_color), mBatteryStyle, true);
         private View mCurrentView;
 
         @Override