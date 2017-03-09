2d6ee76b0703e71dd57fb70a7db7e4ae9de2242a
Alex Cruz <mazdarider23@gmail.com>
2017-03-09 11:01:41 +0000

    Fw_base - [QUICK SETTINGS] Add Caffeine QS Tile
    Change-Id: I5d4aa97ae63fc0bcc40f0fa7d9305efbba091029
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
