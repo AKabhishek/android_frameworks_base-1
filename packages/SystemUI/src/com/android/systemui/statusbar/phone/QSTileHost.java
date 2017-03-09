c61590f919862c00946324bca9ad6ec61f9102d5
Alex Cruz <mazdarider23@gmail.com>
2017-03-09 11:03:35 +0000

    QS: Add Pulse Tile
    Change-Id: If54c630344139cb7218ab48e90a748407e1a87b9
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -60,6 +60,7 @@ import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.PulseTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
