22a44484b088e09d54f52153fad5128856ee6125
Alex Cruz <mazdarider23@gmail.com>
2017-03-09 11:00:22 +0000

    Fw_base - [QUICK SETTINGS] Add Reboot/Recovery QS Tile
    Change-Id: I2c7956b7201e2103420fc7254051141807024fc4
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SyncTile;
