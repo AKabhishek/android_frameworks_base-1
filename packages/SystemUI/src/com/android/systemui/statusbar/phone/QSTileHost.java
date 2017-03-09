e194749c09016f4de6959ea5eaeb41aa6f5c4806
Alex Cruz <mazdarider23@gmail.com>
2017-03-09 11:01:25 +0000

    Fw_base - [QUICK SETTINGS] Add IME selector QS Tile
    Change-Id: I48fd585901cecd24b0ba94409e69b6a5cda907b1
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
+import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
